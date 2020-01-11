f738f77696f758d8044dc8b9e22bdb06f3b93c73
Vishal <Vishalgrover88@gmail.com>
2020-01-11 15:30:28 +0530

    Relative URL-Template Tagging

---
+++ 4. RelativeURL_Template_Tagging/basic_app/basic_app_urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from basic_app import views
+
+
+# Template Tagging
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('other', views.other, name='other'),
+    path('relative', views.relative_url_template, name='relative'),
+]
