e98fdfa9610974729b691e48594fd77255e5a36d
Vishal <Vishalgrover88@gmail.com>
2020-02-10 07:42:04 +0530

    Adding get,post,put,patch and Delete to API methods

--- 6. WorkingWithRestApi/profiles_api/views.py
+++ 6. WorkingWithRestApi/profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.views import Response
+from rest_framework import status
+from profiles_api import serializers
 
 
 class HelloAPIView (APIView):
     """ Test API View """
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """ Test get function """
@@ -16,4 +19,27 @@ class HelloAPIView (APIView):
 
         return Response({'message':'Hello !', 'an_apiview': an_apiview})
 
-        
+    def post(self,request):
+        """ Hello Message with Name """
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, pk=None):
+        """ Handles updating of an object """
+        return Response({'message':'PUT'})
+
+
+    def patch(self, request, pk=None):
+        """ Handles patching of an object """
+        return Response({'message':'PATCH'})
+
+
+    def delete(self, request, pk=None):
+        """ Handles deleting of an object """
+        return Response({'message':'DELETE'})
