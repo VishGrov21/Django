a0789ca3e21fe9ac16204a4b08dd3fd631cfd92c
Vishal <Vishalgrover88@gmail.com>
2020-02-17 10:21:04 +0530

    Added ProfilesViewSet along with permissions classes

--- 6. WorkingWithRestApi/profiles_api/profiles_api_urls.py
+++ 6. WorkingWithRestApi/profiles_api/profiles_api_urls.py
@@ -1,12 +1,13 @@
-from django.urls import path,include
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from profiles_api import views
 
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
-path('hello',views.HelloAPIView.as_view(), name='hello'),
-path('',include(router.urls)),
+    path('hello', views.HelloAPIView.as_view(), name='hello'),
+    path('', include(router.urls)),
 ]
--- 6. WorkingWithRestApi/profiles_api/views.py
+++ 6. WorkingWithRestApi/profiles_api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.views import APIView
-from rest_framework.views import Response
+from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers
+from profiles_api import serializers, models
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
 
 
 class HelloAPIView (APIView):
@@ -12,38 +14,35 @@ class HelloAPIView (APIView):
     def get(self, request, format=None):
         """ Test get function """
         an_apiview = [
-        'Uses HTTP Methods such as (get, post, put, patch, update, delete)',
-        'Is Traditional to django view',
-        'Gives most control over application logic',
-        'Is mapped manually to URLs',
+            'Uses HTTP Methods such as (get, post, put, patch, update, delete)',
+            'Is Traditional to django view',
+            'Gives most control over application logic',
+            'Is mapped manually to URLs',
         ]
 
-        return Response({'message':'Hello !', 'an_apiview': an_apiview})
+        return Response({'message': 'Hello !', 'an_apiview': an_apiview})
 
-    def post(self,request):
+    def post(self, request):
         """ Hello Message with Name """
-        serializer = self.serializer_class(data = request.data)
+        serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
             message = f'Hello {name}'
-            return Response({'message':message})
+            return Response({'message': message})
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
     def put(self, request, pk=None):
         """ Handles updating of an object """
-        return Response({'message':'PUT'})
-
+        return Response({'message': 'PUT'})
 
     def patch(self, request, pk=None):
         """ Handles patching of an object """
-        return Response({'message':'PATCH'})
-
+        return Response({'message': 'PATCH'})
 
     def delete(self, request, pk=None):
         """ Handles deleting of an object """
-        return Response({'message':'DELETE'})
+        return Response({'message': 'DELETE'})
 
 
 class HelloViewSet(viewsets.ViewSet):
@@ -54,15 +53,15 @@ class HelloViewSet(viewsets.ViewSet):
     def list(self, request):
         """ Returns a Hello Message """
         a_viewset = [
-        'Uses actions such as (list, create, retrieve, update, partial_update, delete)',
-        'Automaticaaly maps to URLs using Routers',
-        'Provides more functionality with lesser code'
+            'Uses actions such as (list, create, retrieve, update, partial_update, delete)',
+            'Automaticaaly maps to URLs using Routers',
+            'Provides more functionality with lesser code'
         ]
-        return Response({'message':'Hello', 'a_viewset':a_viewset})
+        return Response({'message': 'Hello', 'a_viewset': a_viewset})
 
     def retrieve(self, request, pk=None):
         """ Handles getting an object by its ID """
-        return Response({'HTTP_METHOD':'GET'})
+        return Response({'HTTP_METHOD': 'GET'})
 
     def create(self, request):
         """ Create a new Hello Message """
@@ -70,18 +69,28 @@ class HelloViewSet(viewsets.ViewSet):
         if serializer.is_valid():
             name = serializer.data.get('name')
             message = f'Hello {name}!'
-            return Response({'message':message})
+            return Response({'message': message})
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def update(self, request, pk=None):
         """ Handles getting an object by its ID """
-        return Response({'HTTP_METHOD':'PUT'})
+        return Response({'HTTP_METHOD': 'PUT'})
 
     def partial_update(self, request, pk=None):
         """ Handles getting an object by its ID """
-        return Response({'HTTP_METHOD':'patch'})
+        return Response({'HTTP_METHOD': 'patch'})
 
     def destroy(self, request, pk=None):
         """ Handles getting an object by its ID """
-        return Response({'HTTP_METHOD':'delete'})
+        return Response({'HTTP_METHOD': 'delete'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """ Handles creating and updating profiles """
+    serializer_class = serializers.UserProfileSerializer
+    # django rest framework is aware of the standard functions that developer would like to
+    # perform on a model viewset, like Create, list, update, partial update
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
